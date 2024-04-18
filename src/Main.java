e16b2eb8924587f18f9b72a0c51ab19e0d3a356f
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-18 02:07:15 -0300

    Learning primitive variable types

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int     anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double  notaDoFilme = 8.1;
+
+        System.out.printf("Ano de lançamento: %d", anoDeLancamento);
+    }
+}
